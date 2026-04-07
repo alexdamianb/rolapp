d003a6799de342e544270260f749df3f1ed1a663
Alexander <alexandertg.busse@gmail.com>
2026-04-07 23:08:11 +0200

    iabasta

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,18 @@
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import firebaseConfig from "../firebase-config.json";
 
+
 const app = initializeApp(firebaseConfig);
+
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getDatabase(app);
+
+
+export const db = getDatabase(
+  app,
+  "https://gen-lang-client-0641922617-default-rtdb.europe-west1.firebasedatabase.app"
+);
